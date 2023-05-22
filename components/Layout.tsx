95fea6927babae0fd2998872ebfdddc6a4a3e9af
Robert Constantin <onepads0@gmail.com>
2023-05-22 16:07:56 +0300

    add overflow

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div className="relative min-h-screen flex flex-col justify-center items-center bg-hero bg-cover">
+  <div className="relative min-h-screen flex flex-col justify-center items-center bg-hero bg-cover overflow-y-hidden">
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
